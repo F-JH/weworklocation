4180d4fa22c9567fa0bfcd1ae6a1b5c23314ffe8
fujuhong <xiaoandi@shopline.com>
2024-03-07 19:25:35 +0800

    injection脚本检查theos是否安装

--- scripts/injection.sh
+++ scripts/injection.sh
@@ -3,6 +3,16 @@ if [ ! -n "$1" ]; then
   echo "Usage: ./injection.sh app.ipa"
   exit 1
 fi
+# 检查theos
+if [ -z "$THEOS" ]; then
+  if [ ! -d "/opt/theos" ]; then
+    echo "Could not find THEOS, please confirm your theos is installed."
+    exit 1
+  else
+    THEOS=/opt/theos
+  fi
+fi
+
 
 BUILD_DIR=var
 
@@ -12,7 +22,7 @@ fi
 unzip -qd $BUILD_DIR/ $1
 APPNAME=`ls $BUILD_DIR/Payload/ | awk -F '.' '{print $1}'`
 
-cp /opt/theos/lib/libsubstrate.dylib $BUILD_DIR/
+cp $THEOS/lib/libsubstrate.dylib $BUILD_DIR/
 cp .theos/obj/debug/weworklocationhook.dylib $BUILD_DIR/
 install_name_tool -change /Library/Frameworks/CydiaSubstrate.framework/CydiaSubstrate @executable_path/Frameworks/libsubstrate.dylib $BUILD_DIR/weworklocationhook.dylib
 optool install -c load -p @executable_path/Frameworks/weworklocationhook.dylib -t $BUILD_DIR/Payload/$APPNAME.app/$APPNAME
